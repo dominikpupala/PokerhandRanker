35c68665672ddc49aa00db19e7aeac40118d87c0
Dominik Pupala <dominik.pupala@gmail.com>
2020-07-08 13:30:11 +0200

    edit card object tostring refactor

--- Card.cs
+++ Card.cs
@@ -4,20 +4,14 @@
     {
         public Card(CardValue value, CardSuit suit)
         {
-            Suit = suit; 
+            Suit = suit;
             Value = value;
         }
 
         public CardSuit Suit { get; set; }
-        
+
         public CardValue Value { get; set; }
 
-        public override string ToString()
-        {
-            // string interpolation syntax
-            // enum is custom made object
-            // no override for ToString()
-            return $"{Value} of {Suit}";
-        }
+        public override string ToString() =>  $"{Value} of {Suit}";
     }
 }
