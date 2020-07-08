70e64744d3c2baa48a78e3a7ec5ff22c109f6a3c
Dominik Pupala <dominik.pupala@gmail.com>
2020-07-08 13:01:32 +0200

    add first test

---
+++ Tests/CardTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace PokerhandRanker.Tests
+{
+    public class CardTests
+    {
+        [Fact]
+        public void CanCreateCard()
+        {
+            object card = new Card();
+            Assert.NotNull(card);
+        }
+    }
+}
