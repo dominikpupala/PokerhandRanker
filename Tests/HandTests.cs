986d01f88dd5d335946766f5c36c5bbac55813ac
Dominik Pupala <dominik.pupala@gmail.com>
2020-07-08 13:57:13 +0200

    add handrank tests

--- Tests/HandTests.cs
+++ Tests/HandTests.cs
@@ -38,5 +38,46 @@ namespace PokerhandRanker.Tests
 
             Assert.Equal(CardValue.King, hand.HighCard());
         }
+
+        [Fact]
+        public void CanScoreHighCard()
+        {
+            Hand hand = new Hand();
+
+            hand.Draw(new Card(CardValue.Seven, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Clubs));
+            hand.Draw(new Card(CardValue.Five, CardSuit.Hearts));
+            hand.Draw(new Card(CardValue.King, CardSuit.Hearts));
+            hand.Draw(new Card(CardValue.Two, CardSuit.Hearts));
+
+            Assert.Equal(HandRank.HighCard, hand.GetHandRank());
+        }
+
+        [Fact]
+        public void CanScoreFlush()
+        {
+            Hand hand = new Hand();
+
+            hand.Draw(new Card(CardValue.Two, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Three, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Ace, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Five, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Six, CardSuit.Spades));
+
+            Assert.Equal(HandRank.Flush, hand.GetHandRank());
+        }
+        [Fact]
+        public void CanScoreRoyalFlush()
+        {
+            Hand hand = new Hand();
+
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Jack, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Queen, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.King, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Ace, CardSuit.Spades));
+
+            Assert.Equal(HandRank.RoyalFlush, hand.GetHandRank());
+        }
     }
 }
