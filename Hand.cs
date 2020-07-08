a200c485c602aaafdc997ea64c815b5f175ab458
Dominik Pupala <dominik.pupala@gmail.com>
2020-07-08 13:44:18 +0200

    add hand object

---
+++ Hand.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace PokerhandRanker
+{
+    public class Hand
+    {
+        public List<Card> Cards { get; }
+
+        public Hand() => Cards = new List<Card>();
+
+        public void Draw(Card card) => Cards.Add(card);
+    }
+}
