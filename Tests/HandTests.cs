e0d658cbda37d92acc00bc13b8fb00cfc22eed5e
Dominik Pupala <dominik.pupala@gmail.com>
2020-07-08 13:39:33 +0200

    add hand tests

---
+++ Tests/HandTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace PokerhandRanker.Tests
+{
+    public class HandTests
+    {
+        [Fact]
+        public void CanCreateHand()
+        {
+            Hand hand = new Hand();
+
+            Assert.False(hand.Cards.Any());
+        }
+
+        [Fact]
+        public void CanHandDrawCard()
+        {
+            Card card = new Card(CardValue.Queen, CardSuit.Hearts);
+            Hand hand = new Hand();
+
+            hand.Draw(card);
+
+            Assert.Equal(hand.Cards.First(), card);
+        }
+    }
+}
