339d7bcfc1e4a51205d34a9761493c89508cb717
Dominik Pupala <dominik.pupala@gmail.com>
2020-07-08 20:43:46 +0200

    edit slight refactor of card object

--- Card.cs
+++ Card.cs
@@ -2,18 +2,16 @@
 {
     public class Card
     {
+        public CardSuit Suit { get; }
+
+        public CardValue Value { get; }
+
         public Card(CardValue value, CardSuit suit)
         {
             Suit = suit;
             Value = value;
         }
 
-        public CardSuit Suit { get; }
-
-        public CardValue Value { get; }
-
         public override string ToString() =>  $"{Value} of {Suit}";
-
-
     }
 }
