cb64aede0375ab74ed93eb17e763fef26d3a0850
Dominik Pupala <dominik.pupala@gmail.com>
2020-07-08 14:31:37 +0200

    add gethandrank method

--- Hand.cs
+++ Hand.cs
@@ -12,5 +12,13 @@ namespace PokerhandRanker
         public void Draw(Card card) => Cards.Add(card);
 
         public CardValue HighCard() => Cards.Max(x => x.Value);
+
+        public HandRank GetHandRank() => 
+            HasRoyalFlush() ? HandRank.RoyalFlush : 
+            HasFlush() ? HandRank.Flush : HandRank.HighCard;
+
+        private bool HasFlush() => Cards.All(x => Cards.First().Suit == x.Suit);
+
+        private bool HasRoyalFlush() => HasFlush() && Cards.All(x => x.Value > CardValue.Nine);
     }
 }
