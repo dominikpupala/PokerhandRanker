829c3d90d293c9da26e68a57d594753a78bd31f6
Dominik Pupala <dominik.pupala@gmail.com>
2020-07-09 14:11:54 +0200

    refactor hand evaluating

---
+++ FiveCardPokerScorer.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace PokerhandRanker
+{
+    public static class FiveCardPokerScorer
+    {
+        public static Card HighCard(IEnumerable<Card> cards) => cards.Aggregate((x, y) => y.Value > x.Value ? y : x);
+
+        public static HandRank GetHandRank(IEnumerable<Card> cards) =>
+            HasRoyalFlush(cards) ? HandRank.RoyalFlush :
+            HasFourOfKind(cards) ? HandRank.FourOfAKind :
+            HasFullHouse(cards) ? HandRank.FullHouse :
+            HasFlush(cards) ? HandRank.Flush :
+            HasStraight(cards) ? HandRank.Straight :
+            HasThreeOfKind(cards) ? HandRank.ThreeOfAKind :
+            HasPair(cards) ? HandRank.Pair : HandRank.HighCard;
+
+        private static bool HasRoyalFlush(IEnumerable<Card> cards) => HasFlush(cards) && cards.All(x => x.Value > CardValue.Nine);
+
+        private static bool HasFlush(IEnumerable<Card> cards) => cards.All(x => cards.First().Suit == x.Suit);
+
+        private static bool HasStraight(IEnumerable<Card> cards) => cards.OrderByDescending(x => x.Value).SelectConsecutive((x, y) => x.Value == y.Value + 1).All(v => v);
+
+        private static bool HasFullHouse(IEnumerable<Card> cards) => HasThreeOfKind(cards) && HasPair(cards);
+
+        private static bool HasFourOfKind(IEnumerable<Card> cards) => HasNumberOfKind(cards, 4);
+
+        private static bool HasThreeOfKind(IEnumerable<Card> cards) => HasNumberOfKind(cards, 3);
+
+        private static bool HasPair(IEnumerable<Card> cards) => HasNumberOfKind(cards, 2);
+
+        private static bool HasNumberOfKind(IEnumerable<Card> cards, int n) => cards.QuantitiesOfKind().Any(i => i.Value == n);
+    }
+}
--- Hand.cs
+++ Hand.cs
@@ -8,32 +8,5 @@ namespace PokerhandRanker
         public List<Card> Cards { get; } = new List<Card>();
 
         public void Draw(Card card) => Cards.Add(card);
-
-        public Card HighCard() => Cards.Aggregate((x, y) => y.Value > x.Value ? y : x);
-
-        public HandRank GetHandRank() =>
-            HasRoyalFlush() ? HandRank.RoyalFlush :
-            HasFourOfKind() ? HandRank.FourOfAKind :
-            HasFullHouse() ? HandRank.FullHouse :
-            HasFlush() ? HandRank.Flush :
-            HasStraight() ? HandRank.Straight :
-            HasThreeOfKind() ? HandRank.ThreeOfAKind :
-            HasPair() ? HandRank.Pair : HandRank.HighCard;
-
-        private bool HasRoyalFlush() => HasFlush() && Cards.All(x => x.Value > CardValue.Nine);
-
-        private bool HasFlush() => Cards.All(x => Cards.First().Suit == x.Suit);
-
-        private bool HasStraight() => Cards.OrderByDescending(x => x.Value).SelectConsecutive((x, y) => x.Value == y.Value + 1).All(v => v);
-
-        private bool HasFullHouse() => HasThreeOfKind() && HasPair();
-
-        private bool HasFourOfKind() => HasNumberOfKind(4);
-
-        private bool HasThreeOfKind() => HasNumberOfKind(3);
-
-        private bool HasPair() => HasNumberOfKind(2);
-
-        private bool HasNumberOfKind(int n) => Cards.QuantitiesOfKind().Any(i => i.Value == n);
     }
 }
